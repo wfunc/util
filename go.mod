546afdabf3c8c3912016cdefa7b261899e9b02eb
wfunc <114468522+wfunc@users.noreply.github.com>
2024-12-14 19:17:18 +0800

    feat: add NamedRunnerWithHMS

--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module github.com/wfunc/util
 
 go 1.23
 
-require golang.org/x/net v0.31.0
+require (
+	github.com/codingeasygo/crud v0.0.0-20230604050204-4e7316aa4767
+	golang.org/x/net v0.31.0
+)
+
+require (
+	github.com/codingeasygo/util v0.0.0-20230604045651-019e6b72f8e5 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx/v4 v4.16.1 // indirect
+	github.com/jackc/puddle v1.2.1 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
--- runner/runner.go
+++ runner/runner.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log"
 	"time"
+
+	"github.com/codingeasygo/crud/pgx"
+	"github.com/wfunc/util/xdebug"
 )
 
-//ErrNotTask is error for not task
+// ErrNotTask is error for not task
 var ErrNotTask = fmt.Errorf("not task")
 
-//NamedRunner will run call by delay
+// NamedRunner will run call by delay
 func NamedRunner(name string, delay time.Duration, running *bool, call func() error) {
 	log.Printf("%v is starting", name)
 	var finishCount = 0
@@ -28,4 +31,58 @@ func NamedRunner(name string, delay time.Duration, running *bool, call func() er
 		time.Sleep(delay)
 	}
 	log.Printf("%v is stopped", name)
+}
+
+func NamedRunnerWithHMS(name string, hour, minute, second int64, running *bool, call func() error) {
+	log.Printf("NamedRunnerWithHMS(%v) is starting", name)
+
+	runCall := func() error {
+		defer func() {
+			if perr := recover(); perr != nil {
+				log.Printf("NamedRunnerWithHMS(%v) is panic with %v, callstack is \n%v", name, perr, xdebug.CallStack())
+			}
+		}()
+		return call()
+	}
+
+	var finishCount = 0
+	first := NextDiff(hour, minute, second)
+	log.Printf("NamedRunnerWithHMS(%v) first run scheduled at %v (in %v)", name, time.Now().Add(first), first)
+	time.Sleep(first) // Sleep until the first scheduled run
+
+	for *running {
+		err := runCall()
+		if err == nil || err == pgx.ErrNoRows {
+			log.Printf("NamedRunnerWithHMS(%v) finished task successfully (%v times)", name, finishCount)
+		} else {
+			finishCount++
+			log.Printf("NamedRunnerWithHMS(%v) task failed with error: %v (%v times)", name, err, finishCount)
+			if finishCount > 100 {
+				break
+			}
+			continue
+		}
+
+		nextDiff := NextDiff(hour, minute, second)
+		log.Printf("NamedRunnerWithHMS(%v) next run scheduled in %v (at %v)", name, nextDiff, time.Now().Add(nextDiff))
+		time.Sleep(nextDiff)
+	}
+
+	log.Printf("NamedRunnerWithHMS(%v) has stopped", name)
+}
+
+// 指定时间 x 时 x 分 x 秒，计算与当前时间的时间差
+func NextDiff(hour, minute, second int64) time.Duration {
+	targetSeconds := hour*3600 + minute*60 + second
+	now := time.Now()
+	currentSeconds := int64(now.Hour()*3600 + now.Minute()*60 + now.Second())
+
+	var diffSeconds int64
+	if targetSeconds > currentSeconds {
+		diffSeconds = targetSeconds - currentSeconds
+	} else {
+		diffSeconds = 86400 - currentSeconds + targetSeconds // 跨天的情况
+	}
+
+	return time.Duration(diffSeconds) * time.Second
 }
